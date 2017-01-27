cd051d26ef458090ed168db2d83b152cafae839b
Perry Quinn <perry@missioncontrolinc.com>
2017-01-27 10:05:55 -0800

    Setup start script and Heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -19,6 +20,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
     res.render('maintenance.hbs');
 });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -49,6 +51,6 @@ app.get('/bad', (req, res) => {
     res.send({errorMessage: 'bad response :('});
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
